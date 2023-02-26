7474c45417d66d8c92799dc36e588c4ccf84d3da
kubaz <jakub.zawadzki16@gmail.com>
2023-02-26 22:02:21 +0100

    Decided to switch to the thymeleaf from angular, created module castle-thymeleaf-frontend and added it as submodule to the castle module.

--- src/main/java/com/castle/thymeleaffrontend/CastleThymeleafFrontendApplication.java
+++ src/main/java/com/castle/thymeleaffrontend/CastleThymeleafFrontendApplication.java
@@ -3,11 +3,11 @@ package com.castle.thymeleaffrontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.castle")
 public class CastleThymeleafFrontendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CastleThymeleafFrontendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CastleThymeleafFrontendApplication.class, args);
+    }
 
 }
--- src/main/java/com/castle/thymeleaffrontend/controllers/ThymeleafController.java
+++ src/main/java/com/castle/thymeleaffrontend/controllers/ThymeleafController.java
@@ -1,2 +1,20 @@
-package com.castle.thymeleaffrontend.controllers;public class ThymeleafController {
+package com.castle.thymeleaffrontend.controllers;
+
+import com.castle.webapi.services.IWeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ThymeleafController {
+
+    private final IWeatherService weatherService;
+
+    @GetMapping("/home")
+    public String homePage(Model model) {
+        model.addAttribute("weather",weatherService.getLatestCurrentWeatherDto().getBody());
+        return "home";
+    }
 }
